5710b7bcf69cbbae168bd11129c9f480428e1afa
Maneeva Katya <maneeva_katya_@mail.ru>
2019-07-06 22:34:42 +0500

    about, article, blog, publications

---
+++ static/js/script.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function() {
+	setTimeout(function() {
+		$('body').addClass('animate');
+
+		var arrD = ["first", "second", "third"],
+			i = -1,
+			prev = 1;
+		setInterval(function () {
+			arrD[
+				i = ++i % arrD.length
+			];
+			$('body').addClass(arrD[i]);
+			$('body').removeClass(arrD[prev]);
+			prev = i;
+		}, 6500);
+	}, 500)
+
+
+	var mySwiper = new Swiper ('.swiper-container', {
+		direction: 'horizontal',
+		speed: 400,
+		slidesPerView: 3,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		  },
+	  })
+});
